1458385a3521679417267f58cdab610462cc8fc7
Lucas <lucasdsveras@gmail.com>
2023-08-28 00:59:06 +0100

    Change grocerieList state structure and add components

--- grocerie-list/src/App.js
+++ grocerie-list/src/App.js
@@ -1,9 +1,29 @@
 import { useState } from "react";
 
 const initialList = [
-  { category: "Fruits and vegetables", items: ["Banana", "Apple", "Broccoli"] },
-  { category: "Dairy", items: ["Milk", "Yoghurt", "Cheese"] },
-  { category: "", items: ["Broom", "Bucket"] },
+  {
+    category: "Fruits and vegetables",
+    items: [
+      { name: "Banana", checked: true },
+      { name: "Apple", checked: false },
+      { name: "Broccoli", checked: false },
+    ],
+  },
+  {
+    category: "Dairy",
+    items: [
+      { name: "Milk", checked: false },
+      { name: "Yoghurt", checked: false },
+      { name: "Cheese", checked: false },
+    ],
+  },
+  {
+    category: "",
+    items: [
+      { name: "Broom", checked: true },
+      { name: "Bucket", checked: true },
+    ],
+  },
 ];
 
 export default function App() {
@@ -26,6 +46,17 @@ function Header() {
   return (
     <div className="header">
       <h1>🛒 Groceries list</h1>
+      <div className="controls">
+        <Button padding={"0.8rem 1.6rem"} borderRadius={"10px"}>
+          Add Category
+        </Button>
+        <Button padding={"0.8rem 1.6rem"} borderRadius={"10px"}>
+          Add Item
+        </Button>
+        <Button padding={"0.8rem 1.6rem"} borderRadius={"10px"}>
+          Clear List
+        </Button>
+      </div>
     </div>
   );
 }
@@ -36,7 +67,7 @@ function GrocerieList({ list }) {
       <h2>{list.category}</h2>
       <ul className="grocerie-list">
         {list.items.map((item) => (
-          <GrocerieItem item={item} key={item} />
+          <GrocerieItem item={item} key={item.name} />
         ))}
       </ul>
     </div>
@@ -44,5 +75,24 @@ function GrocerieList({ list }) {
 }
 
 function GrocerieItem({ item }) {
-  return <li>{item}</li>;
+  return (
+    <li className="grocerie-item">
+      <input type="checkbox" />
+      {item.name}
+    </li>
+  );
+}
+
+function Button({ padding, borderRadius, children }) {
+  return (
+    <button
+      className="btn"
+      style={{
+        padding: padding,
+        borderRadius: borderRadius,
+      }}
+    >
+      <span className="btn-txt">{children}</span>
+    </button>
+  );
 }
